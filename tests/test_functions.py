5127a26a86c3c595c77946a869f71e5881daff95
AshleyShalloe <54893285+AshleyShalloe@users.noreply.github.com>
2025-10-24 10:15:54 +0100

    Initial commit

---
+++ tests/test_functions.py
@@ -0,0 +1,12 @@
+"""
+Create unit tests for modules in the tests/ folder. All functions in a repo should be unit tested and
+tests should be run before and after any changes are made.
+"""
+
+import pytest  # noqa: F401
+
+from mscape_template import mscape_functions as mf  # noqa: F401
+
+
+def test_example():
+    assert True
